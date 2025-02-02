8db88bf0cff3ce37dc34607c985bcf5011c811eb
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-02-02 17:25:59 -0500

    Pushes

--- Spring_25/CS3630/Project_1/controllers/proj1_vision_only_controller/camera_only.py
+++ Spring_25/CS3630/Project_1/controllers/proj1_vision_only_controller/camera_only.py
@@ -19,15 +19,32 @@ def vision_only_distance_calculation(image, camera_fov, object_diameter):
         angle: the angle of the detected circle in radians.
     """
 
-    ###########################################################################
-    # TODO: Student code begins
-    ###########################################################################
-    
-    # Remove this!
-    raise NotImplementedError
-        
-    ###########################################################################
-    # Student code ends
-    ###########################################################################
+    shape_info = detect_circle(image)
 
-    return depth, angle
+    if shape_info is None or not isinstance(shape_info, (list, tuple, np.ndarray)):
+        return None, None
+
+    if isinstance(shape_info, np.ndarray):
+        shape_info = shape_info.flatten()
+
+    if len(shape_info) != 3:
+        return None, None
+
+    x_center, y_center, radius = shape_info
+    object_diameter_in_image = 2 * float(radius)
+
+    image_width = image.shape[1]
+    focal_length = image_width / (2 * math.tan(camera_fov / 2))
+
+    original_depth = (object_diameter * focal_length) / object_diameter_in_image
+
+    x_translation = 0.03
+    y_translation = 0
+    z_translation = 0.028
+
+    depth = math.sqrt(original_depth**2 + x_translation**2 + y_translation**2 + z_translation**2)
+
+    image_center_x = image_width / 2
+    fin_angle = ((x_center - image_center_x) / image_width) * camera_fov
+
+    return depth, fin_angle
