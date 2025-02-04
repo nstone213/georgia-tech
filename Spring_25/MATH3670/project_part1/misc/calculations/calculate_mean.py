9c5e59890bb7c2d55ea52f6d56d520e3f00e1caf
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-02-03 22:43:59 -0500

    Commenting

--- Spring_25/MATH3670/project_part1/misc/calculations/calculate_mean.py
+++ Spring_25/MATH3670/project_part1/misc/calculations/calculate_mean.py
@@ -1,17 +1,32 @@
+# Imports
 import pandas as pd
 
+#Function
 def calculate_mean(data):
 
+    #Return statement for sample mean
     return sum(data) / len(data) if data else None
 
+#Main code execution
 if __name__ == "__main__":
 
+    #File path (relative)
     file_path = "/Users/nicholasstone/.cache/kagglehub/datasets/luvharishkhati/heart-disease-patients-details/versions/1/heart_disease.csv"
+    
+    #Reads to Pandas datafram
     df = pd.read_csv(file_path)
 
+    #Looking for age in the dataframe
     if "age" in df.columns:
+
+        #Converts age to a list
         data = df["age"].dropna().tolist()
-        mean_value = calculate_mean(data)
-        print(f"Sample Mean: {mean_value}")
+
+        #Calculates sample mean by referring to python file
+        sample_mean_value = calculate_mean(data)
+
+        #Prints to terminal
+        print(f"Sample Mean: {sample_mean_value}")
     else:
+        #'Error' message
         print("The 'age' column is not present in the dataset.")
