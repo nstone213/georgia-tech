0cfef19448ef8ab3ce30a63a42101186c24cf277
nstone213 <148160623+nstone213@users.noreply.github.com>
2025-03-04 23:05:43 -0500

    integer square root

--- Spring_25/CS2110/hw04/integerSquareRoot.asm
+++ Spring_25/CS2110/hw04/integerSquareRoot.asm
@@ -24,24 +24,19 @@
     AND R0, R0, #0   ; L = 0
     ADD R1, R0, #1   ; a = 1
     ADD R2, R0, #3   ; d = 3
-
-    LD R3, N         ; Load N from memory
-    
-LOOP
-    NOT R4, R1       ; Compute -a
+    LD R3, N         ; N
+WHILELOOP
+    NOT R4, R1       ; -a
     ADD R4, R4, #1
-    ADD R4, R4, R3   ; Check if a > N
-    BRN DONE         ; If a > N, exit loop
-
+    ADD R4, R4, R3   ; Check while condition
+    BRN FIN          ; If negative, leave
     ADD R1, R1, R2   ; a = a + d
     ADD R2, R2, #2   ; d = d + 2
     ADD R0, R0, #1   ; L = L + 1
-    BR LOOP          ; Repeat the loop
-
-DONE
-    LD R5, RESULT    ; Load memory address for storing L
-    STR R0, R5, #0   ; Store L at RESULT location
-    
+    BR WHILELOOP     ; Go to top of loop
+FIN
+    LD R5, RESULT    ; L's mem address
+    STR R0, R5, #0   ; Store L at RESULT
     HALT
 
 ;; Do not rename or remove any existing labels
